e939cdaff95d4653efa665ea82effe20d53cf2b9
Eryk J <infiniti@inventati.org>
2022-08-07 08:22:59 -0400

    Update version and description

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ setuptools.setup(
 
     name="linkture",
 
-    version="1.0.0",
+    version="1.2.1",
 
     author="Eryk J.",
 
     author_email="infiniti@inventati.org",
 
-    description="Process Bible references",
+    description="Process and link/code Bible references",
 
-    long_description="Process Bible references and convert them into jwpub links",
+    long_description="Functions to convert scriptures to a list of coded ranges or to .jwpub links. This script *does not* parse text files for scriptures - it only parses what is enclosed within {{ }} or provided as a string argument. Also, it doesn't check if chapters or verses actually exist (within range). Make sure the scriptures use common English names/abbreviations.",
 
     long_description_content_type="text/markdown",
 
