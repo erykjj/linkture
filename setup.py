129d36f460d5e058652ba06e6c45d1b2f56452fe
Eryk J <infiniti@inventati.org>
2022-08-07 21:06:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,28 @@
 import setuptools
+from pathlib import Path
+
+work_dir = Path(__file__).parent
+long_descr = (work_dir / "README.md").read_text()
 
 setuptools.setup(
 
     name="linkture",
-
-    version="1.2.1",
-
+    version="1.2.2",
     author="Eryk J.",
-
     author_email="infiniti@inventati.org",
+    url="https://github.com/erykjj/linkture",
 
     description="Process and link/code Bible references",
+    long_description = long_descr
+    long_description_content_type="text/markdown"
 
-    long_description="Functions to convert scriptures to a list of coded ranges or to .jwpub links. This script *does not* parse text files for scriptures - it only parses what is enclosed within {{ }} or provided as a string argument. Also, it doesn't check if chapters or verses actually exist (within range). Make sure the scriptures use common English names/abbreviations.",
-
-    long_description_content_type="text/markdown",
-
-    url="https://github.com/erykjj",
 
     packages=setuptools.find_packages(),
-
     classifiers=[
-
         "Programming Language :: Python :: 3",
-
         "License :: OSI Approved :: MIT License",
-
         "Operating System :: OS Independent",
-
     ],
-
     python_requires='>=3.6',
 
 )
