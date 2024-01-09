23a313ca3fb6521aeb82344018d70251c29e0383
Eryk J. <107011362+erykjj@users.noreply.github.com>
2024-01-09 08:07:13 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (work_dir / "README.md").read_text()
 setuptools.setup(
 
     name="linkture",
-    version="2.4.2",
+    version="2.4.3",
     author="Eryk J.",
     url="https://github.com/erykjj/linkture",
 
