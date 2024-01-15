50cc86273ef776992e843a665292d22d688d09af
Eryk J. <107011362+erykjj@users.noreply.github.com>
2024-01-15 09:49:49 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (work_dir / "README.md").read_text()
 setuptools.setup(
 
     name="linkture",
-    version="2.4.5",
+    version="2.5.0",
     author="Eryk J.",
     url="https://github.com/erykjj/linkture",
 
